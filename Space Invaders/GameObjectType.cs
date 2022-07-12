4fe6087a5f95799bde78dceed6efc55530edfed0
Alexey-Prohorov <52043736+Alexey-Prohorov@users.noreply.github.com>
2022-07-12 16:57:13 +0500

    Init

---
+++ Space Invaders/GameObjectType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Invaders
+{
+    enum GameObjectType
+    {
+        AlienShip = 1,
+        PlayerShoip = 2,
+        PlayerShipMissle = 3,
+        GroundObject = 4,
+    }
+}
---
+++ Space Invaders/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Space_Invaders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
