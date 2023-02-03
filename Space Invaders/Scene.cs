bbf714f05baa2738be2818416561f7a38e018c65
Alex <prokhorovaa@gov74.ru>
2023-02-03 16:02:46 +0500

    Add class "Scene" and "PlayerShipMissile"

--- Space Invaders/Scene.cs
+++ Space Invaders/Scene.cs
@@ -8,5 +8,28 @@ namespace Space_Invaders
 {
     internal class Scene
     {
+        List<GameObject> _swarm;
+
+        List<GameObject> _ground;
+
+        List<GameObject> _playerShip;
+
+        List<GameObject> _playerShipMissile;
+
+        private static Scene _scene;
+
+        private Scene()
+        {
+            
+        }
+
+        public static Scene GetScene()
+        {
+            if (_scene == null)
+            {
+
+            }
+            return _scene;
+        }
     }
 }
